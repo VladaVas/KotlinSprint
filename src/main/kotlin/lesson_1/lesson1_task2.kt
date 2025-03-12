6d564440bd704363f173551b5949fb485ae719e5
Vlada Vasileva <vvavlada@gmail.com>
2025-03-12 23:08:11 +0300

    KS-1-2 task solution

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,2 +1,16 @@
 package org.example.lesson_1
 
+fun main() {
+    val numberOfOrders = 75
+    val gratitudeMessage = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(gratitudeMessage)
+
+    var numberOfEmployee = 2000
+//    println(numberOfEmployee)
+
+    numberOfEmployee = 2000 - 1
+    println(numberOfEmployee)
+
+}
