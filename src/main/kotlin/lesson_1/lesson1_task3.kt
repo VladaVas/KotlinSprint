ad8908df7ca9bb3941aaa904b088c8fbd0e67164
Vlada Vasileva <vvavlada@gmail.com>
2025-03-12 23:35:00 +0300

    KS-1-3 task solution

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,2 +1,22 @@
 package org.example.lesson_1
 
+fun main() {
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+//    println(hour)
+//    println(minute)
+
+    hour = 10
+    minute = 55
+    val colon = ":"
+    print(hour)
+    print(colon)
+    print(minute)
+
+
+
+}
