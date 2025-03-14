608dfc48046b644b89b7d3064b55fec4ce2d59bc
Vlada Vasileva <vvavlada@gmail.com>
2025-03-14 23:23:23 +0300

    KS-2-5 task solution

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -8,8 +8,6 @@ fun main() {
     val annualInterestRate = 16.7 / 100
     val years = 20
 
-    // Формула сложных процентов: A = P * (1 + r)^n
-
     val finalAmount = initialAmount * (1 + annualInterestRate).pow(years)
 
     println("Размер вклада через 20 лет: %.3f".format(finalAmount))
