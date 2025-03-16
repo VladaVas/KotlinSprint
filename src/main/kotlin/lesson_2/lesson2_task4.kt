ad050fa009b61995ad885915eedfc9308035d098
Vlada Vasileva <vvavlada@gmail.com>
2025-03-17 00:00:16 +0300

    KS-2-3 вынесла 60 в константу

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,10 +4,13 @@ fun main() {
 
     val crystalOre = 7
     val ironOre = 11
-    val buffMultiplier = 0.2
+    val buffPercentage = 20
+    val buffMultiplier = buffPercentage / 100.0
 
     val bonusCrystalOre = (crystalOre * buffMultiplier).toInt()
     val bonusIronOre = (ironOre * buffMultiplier).toInt()
+
+    println(buffMultiplier)
 
     println("Бонусные кристаллические руды: $bonusCrystalOre")
     println("Бонусные железные руды: $bonusIronOre")
