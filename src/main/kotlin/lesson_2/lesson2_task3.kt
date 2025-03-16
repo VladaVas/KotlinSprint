11defc0a33226b3f21ab6e323a7a7cf031f9e921
Vlada Vasileva <vvavlada@gmail.com>
2025-03-16 23:48:10 +0300

    KS-2-3 вынесла 60 в константу

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -2,14 +2,16 @@ package org.example.lesson_2
 
 fun main() {
 
+    val MINUTES_IN_HOUR = 60
+
     val departureHour = 9
     val departureMinute = 39
     val travelTimeMinutes = 457
 
-    val totalMinutes = departureHour * 60 + departureMinute + travelTimeMinutes
+    val totalMinutes = departureHour * MINUTES_IN_HOUR + departureMinute + travelTimeMinutes
 
-    val arrivalHour = (totalMinutes / 60) % 24
-    val arrivalMinute = totalMinutes % 60
+    val arrivalHour = (totalMinutes / MINUTES_IN_HOUR) % 24
+    val arrivalMinute = totalMinutes % MINUTES_IN_HOUR
 
     println("Поезд прибудет в $arrivalHour:$arrivalMinute")
 }
