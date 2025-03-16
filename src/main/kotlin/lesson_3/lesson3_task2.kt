d1e604391af826778559143c4b40100e40537c4a
Vlada Vasileva <vvavlada@gmail.com>
2025-03-17 01:22:07 +0300

    KS-3-2 решение задачи

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var userName = "Андреева Татьяна Сергеевна"
+    var userNameAge = 20
+    println("$userName, $userNameAge")
+
+    userName = "Сидорова Татьяна Сергеевна"
+    userNameAge = 22
+    println("$userName, $userNameAge")
+
+
+
+
+}
