c617fd65f2a8987bba575be6f3f826745a1d8d50
Vlada Vasileva <vvavlada@gmail.com>
2025-03-17 01:58:37 +0300

    KS-3-5 решение задачи

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val from = "D2"
+    val to = "D4"
+    val movementMumber = 0
+
+    println(from)
+    println(to)
+    println(movementMumber)
+}
