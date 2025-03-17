b0804776db1ab471fcb92147a070872343ba7158
Vlada Vasileva <vvavlada@gmail.com>
2025-03-18 00:24:17 +0300

    KS-1-2 пытаюсь исправить merge

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за ваш заказ! Мы ценим ваше доверие и надеемся на дальнейшее сотрудничество!"
+
+    println("Количество заказов: $numberOfOrders")
+    println("Сообщение: $thankYouMessage")
+
+    var numberOfEmployees: Int = 2000
+    println("Количество работников: $numberOfEmployees")
+
+    // println("Количество работников: $numberOfEmployees")
+
+    numberOfEmployees = 1999
+    println("Количество работников после увольнения: $numberOfEmployees")
+}
