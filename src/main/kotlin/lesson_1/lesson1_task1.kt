5335d3cf4d9cee8e6221866dd1adbcc6a311bbf9
Vlada Vasileva <vvavlada@gmail.com>
2025-03-17 23:43:53 +0300

    KS-1-1/2

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson_1
-
-fun main() {
-    val numberOfOrders = 75
-    val gratitudeMessage = "Спасибо за покупку!"
-
-    println(numberOfOrders)
-    println(gratitudeMessage)
-}
