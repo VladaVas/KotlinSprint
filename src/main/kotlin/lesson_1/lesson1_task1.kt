695152ca728288bbc389cbfab34ab6431a2fc6fe
Vlada Vasileva <vvavlada@gmail.com>
2025-03-18 00:19:16 +0300

    KS-1-1 пытаюсь исправить merge

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+
+    val thankYouMessage: String = "Спасибо за ваш заказ! Мы ценим ваше доверие и надеемся на дальнейшее сотрудничество!"
+
+    println("Количество заказов: $numberOfOrders")
+    println(thankYouMessage)
+}
